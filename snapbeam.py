d3414cc90580e3ad0d6b24e67ca52973498a9dd9
Jaytohe <jaytohe@github.com>
2021-10-07 23:54:20 +0100

    New: CmdLine args main file.
    - This should be run from the terminal in order to use snapbeam. It is
      the so called "main" method in Java terms.

--- snapbeam.py
+++ snapbeam.py
@@ -1,5 +1,6 @@
 import argparse
 from automator_setup import Setup
+from main_payloads import PayloadRunner
 '''
 -h / --help : show help message
 
@@ -25,6 +26,17 @@ parser.add_argument(
     const="",
     help="Setup automation env by finding the positions of buttons in Snapchat."
 )
+
+parser.add_argument(
+    '--file',
+    dest='filename',
+    action='store',
+    metavar='filename',
+    default=None,
+    type=str,
+    help="Name of JSON file that contains the positions of screen elements."
+)
+
 
 parser.add_argument(
     '--boost',
@@ -56,37 +68,55 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-if (args.setupfname is not None
-        and args.cycles is None
-        and args.usernames is None
-        and not args.removeall
-        ):
-    env_prepper = Setup(host="127.0.0.1", port=5037)
-    env_prepper.setup_automation_env(setup_mode=0, file_output=args.setupfname)
+if (args.setupfname is not None):
+            if (args.cycles is None
+            and args.usernames is None
+            and not args.removeall
+            ):
+                env_prepper = Setup(host="127.0.0.1", port=5037)
+                env_prepper.setup_automation_env(setup_mode=0, file_output=args.setupfname)
 
-elif (args.setupfname is not None
-        and args.cycles is not None
-        and args.usernames is None
-        and not args.removeall
-        ):
-    if not args.setupfname: # If string is indeed empty
-        env_prepper = Setup(host="127.0.0.1", port=5037)
-        env_prepper.setup_automation_env(setup_mode=1)
+            if (args.cycles is not None
+                    and args.usernames is None
+                    and not args.removeall
+            ):
+                if not args.setupfname: # If string is indeed empty
+                        env_prepper = Setup(host="127.0.0.1", port=5037)
+                        env_prepper.setup_automation_env(setup_mode=1)
 
-elif (args.setupfname is not None
-        and args.cycles is None
-        and args.usernames is not None
-        and not args.removeall
-        ):
-    if not args.setupfname: # If string is indeed empty
-        env_prepper = Setup(host="127.0.0.1", port=5037)
-        env_prepper.setup_automation_env(setup_mode=2)
+                        runner = PayloadRunner("127.0.0.1", 5037, env_prepper.positions)
+                        runner.boost_score_routine(args.cycles)
+
+            if (args.cycles is None
+            and args.usernames is not None
+            and not args.removeall
+            ):
+                if not args.setupfname: # If string is indeed empty
+                    env_prepper = Setup(host="127.0.0.1", port=5037)
+                    env_prepper.setup_automation_env(setup_mode=2)
 
 elif (args.removeall 
         and args.setupfname is None
         and args.cycles is None
         and args.usernames is None
         ):
-        #delete all friends here
-        pass
+            runner = PayloadRunner("127.0.0.1", 5037, dict()) #removeall friends is purely hardcoded atm so no known positions are provided.
+            runner.delete_all_friends_routine()
+
+elif(args.cycles
+        and args.setupfname is None
+        and args.usernames is None
+        and args.filename is not None
+        ):
+            runner = PayloadRunner("127.0.0.1", 5037, args.filename)
+            runner.boost_score_routine(args.cycles)
+
+elif(args.usernames
+        and args.setupfname is None
+        and args.cycles is None
+        and args.filename is not None
+        ):
+            runner = PayloadRunner("127.0.0.1", 5037, args.filename)
+            runner.add_friends_routine(args.usernames)
+
 
